99f6748d30dfadbe0a54f552a1fc2fb3fa8cf2b4
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-05 22:56:30 +0500

    Update Cards.jsx

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -8,6 +8,10 @@ import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 
 // Import Style File
 import styles from './Cards.module.css';
+
+// Import Classnames for giving multiple classes to component
+import cx from 'classnames';
+
 const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
 
     if (!confirmed) {
@@ -16,7 +20,7 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
     return (
         <div className={styles.container}>
             <Grid container spacing={3} justify="center" direction="column">
-                <Grid item component={Card}>
+                <Grid item component={Card} className={cx(styles.card, styles.active)}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>ACTIVE CASES</Typography>
                         <Typography variant="h5">
@@ -25,7 +29,7 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
                         <Typography color="textSecondary"> {new Date(lastUpdate).toDateString()} </Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component={Card}>
+                <Grid item component={Card} className={cx(styles.card, styles.recovered)}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Recovered</Typography>
                         <Typography variant="h5">
@@ -34,7 +38,7 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
                         <Typography color="textSecondary"> {new Date(lastUpdate).toDateString()} </Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component={Card}>
+                <Grid item component={Card} className={cx(styles.card, styles.deaths)}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Deaths</Typography>
                         <Typography variant="h5">
