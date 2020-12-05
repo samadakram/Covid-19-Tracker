07e635ecdfae8e1cbd214fe728367f57e48bbebe
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-05 21:31:50 +0500

    Update Cards.jsx

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
 
-const Cards = () => {
+// Import Material Ui components
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+
+// Import Style File
+import styles from './Cards.module.css';
+const Cards = ({confirmed, recoverd, deaths, lastUpdate}) => {
+
+    if (!confirmed){
+        return <h1>Loading...</h1>
+    }
     return (
-        <div>
-            <h1>Cards</h1>
+        <div className={styles.container}>
+            <Grid container spacing={3} justify="center" direction="column">
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Active Cases</Typography>
+                        <Typography variant="h5"> {confirmed.value} </Typography>
+                        <Typography color="textSecondary">DATE</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+                        <Typography variant="h5">REAL DATA</Typography>
+                        <Typography color="textSecondary">DATE</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Deaths</Typography>
+                        <Typography variant="h5">REAL DATA</Typography>
+                        <Typography color="textSecondary">DATE</Typography>
+                    </CardContent>
+                </Grid>
+            </Grid>
         </div>
     )
 }
