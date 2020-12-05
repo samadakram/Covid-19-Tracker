bddd192523a5035b920a7293c506e63099fcda01
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-05 23:17:18 +0500

    Update Chart.jsx

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+// Import API
+import { fetchDailyData } from '../../api';
 
 const Chart = () => {
+
+    const [dailyData, setDailyData] = useState({});
+
+    useEffect( ()=>{
+        const fetchAPI = async () => {
+            setDailyData(await fetchDailyData());
+        }
+
+        fetchAPI();
+    },[]);
+
     return (
         <div>
             <h1>Chart</h1>
