ce139e09d264fdb304f2af87ac066107028b45ce
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-06 02:31:58 +0500

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const url = 'https://covid19.mathdro.id/api';
 
-export const fetchData = async () => {
+export const fetchData = async (country) => {
+    let changeableUrl = url;
     try {
         const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
 
