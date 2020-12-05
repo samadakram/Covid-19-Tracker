cf0af901d4bd72a4e1b2add427be7577163f58a9
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-05 23:48:08 +0500

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -18,7 +18,13 @@ export const fetchDailyData = async () => {
     try {
         const {data} = await axios.get(`${url}/daily`);
 
-        console.log(data);
+        const modifiedData = data.map((dailyData)=> ({
+            confirmed: dailyData.confirmed.total,
+            deaths: dailyData.deaths.total,
+            date: dailyData.reportDate,
+        }));
+
+        return modifiedData;
     } catch (error) {
         console.log(error);
     }
